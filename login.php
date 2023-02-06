8e27a8f7e082610cc02883e29adaddc6b84c69c9
Freddo <fredrik.gullin@gmail.com>
2023-02-06 14:22:37 +0100

    added login.php

---
+++ login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="action.php" method="POST">
+        <label for="username">Username</label><br>
+        <input type="text" name="username" id="username"><br>
+        <label for="">Password</label><br>
+        <input type="password" name="password" id="password"><br>
+        <input type="submit" name="login" id="submit"><br>
+    </form>
+
+</body>
+</html>
